ff0d01fe14e5cfd13111ecda819cebc40e56dd67
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-29 12:58:29 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :artists do
-    resources :songs
+  resources :artists, only: [:show, :index] do
+    resources :songs, only: [:show, :index]
   end
   resources :songs do
     resources :artists
